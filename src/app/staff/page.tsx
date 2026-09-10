cb6d1e96095b1e8d33223e1bdaf041c739a3e89e
mayako555 <mayakoinny@gmail.com>
2026-09-10 22:37:15 +0900

    fix: scope staff management to current company

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -181,7 +181,7 @@ export default function StaffPage() {
 
   async function loadStaff() {
     setLoading(true);
-    const data = await getStaffList({ includeResigned: true });
+    const data = await getStaffList({ includeResigned: true, companyScoped: true });
     setStaffList(data);
     setLoading(false);
   }
